169921e894251f60d80e04ddea68328fbd8a5746
Salma-fathi <salma89040@gmail.com>
2023-09-12 14:04:22 -0700

    24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The number to get the last digit from
- *
- * Return: The value of the last digit
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int a;
 
-    last_digit = n % 10;
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (n < 0)
+		n = -n;
 
-    _putchar(last_digit + '0');
-    return last_digit;
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,36 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-    int row, column, product;
+	int i, j, k;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
 
-            if (column != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-
-            if (product < 10)
-                _putchar(' ');
-            else
-                _putchar((product / 10) + '0');
-
-            _putchar((product % 10) + '0');
-        }
-        _putchar('\n');
-    }
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
