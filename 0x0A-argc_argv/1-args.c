72308acc8680c2f30e8bfc81adc8aeec86c6adfc
Salma-fathi <salma89040@gmail.com>
2023-10-18 08:37:37 -0700

    first co

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - Entry point to function
- * @argc: Length of input arguments
- * @argv: vector containing the input arguments
- * Return: 0 if none error
- */
+ * main - a program that prints the number of arguments
+ *        passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i = 0, m;
 
+	while (i < argc)
+	{
+		m = i;
+		i++;
+	}
+	printf("%d\n", m);
 	return (0);
 }
